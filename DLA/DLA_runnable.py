8aacca8853e85fdeea2d2a86c0c22197c3877770
Riccardo Uslenghi <55544485+ruslenghi@users.noreply.github.com>
2020-10-29 13:02:49 +0100

    Add files via upload

---
+++ DLA/DLA_runnable.py
@@ -0,0 +1,33 @@
+from DLA import DLA
+
+N = 16
+l = 51
+g = 300
+a = 0
+
+for k in range(N):
+
+    if a == 0:
+
+        dla1 = DLA(l, 601)
+
+    else:
+
+        dla1 = appo
+
+    for i in range(g):
+        dla1.OnePiece()
+
+    a += 5
+
+    appo = DLA(l+10, 601)
+    appo.t = dla1.t
+
+    for i in range(l):
+        for j in range(l):
+            appo.x[l + 15 + i][l + 15 + j] = dla1.x[l+i][l+j]
+
+    l += 10
+
+dla1.PrintGrid()
+
