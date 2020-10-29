977f12c2ee473d0755b52f9830e7e92cada1c929
Riccardo Uslenghi <55544485+ruslenghi@users.noreply.github.com>
2020-10-29 13:04:41 +0100

    Update DLA_runnable.py

--- DLA/DLA_runnable.py
+++ DLA/DLA_runnable.py
@@ -4,25 +4,17 @@ N = 16
 l = 51
 g = 300
 a = 0
-
 for k in range(N):
-
     if a == 0:
-
         dla1 = DLA(l, 601)
-
     else:
-
         dla1 = appo
-
     for i in range(g):
         dla1.OnePiece()
-
     a += 5
-
+    
     appo = DLA(l+10, 601)
     appo.t = dla1.t
-
     for i in range(l):
         for j in range(l):
             appo.x[l + 15 + i][l + 15 + j] = dla1.x[l+i][l+j]
@@ -30,4 +22,3 @@ for k in range(N):
     l += 10
 
 dla1.PrintGrid()
-
